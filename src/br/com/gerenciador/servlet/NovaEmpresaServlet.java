bf1b9baaa40aa41b76aee6bb53cffe40ca22e261
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2021-02-20 18:42:40 -0300

    Criada nova servlet

---
+++ src/br/com/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,36 @@
+package br.com.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
+@WebServlet("/nova-empresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public NovaEmpresaServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String empresa = request.getParameter("nome");
+		System.out.println("Cadastrando nova empresa: " + empresa);
+		PrintWriter out = response.getWriter();
+		out.println("<html><body><h1>Empresa " + empresa + " cadastrada com sucesso.");
+	}
+
+}
